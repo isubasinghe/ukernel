125188a60fa707be8b71dde94641337d37d8e563
Isitha Subasinghe <isubasinghe@student.unimelb.edu.au>
2022-06-25 13:51:58 +0200

    added more binops for mem types

--- src/memory/types.rs
+++ src/memory/types.rs
@@ -1,7 +1,10 @@
 use core::ops::{
     Add, AddAssign, BitAnd, BitAndAssign, BitOr, BitOrAssign, BitXor, BitXorAssign, Div, Mul, Sub,
-    SubAssign,
+    SubAssign, Rem
 };
+
+use core::convert::{From, Into};
+
 use derive_more::*;
 
 #[derive(
@@ -49,12 +52,15 @@ pub trait MemoryAddress:
     + SubAssign
     + Mul 
     + Div
+    + Rem
     + BitAnd
     + BitAndAssign
     + BitOr
     + BitOrAssign
     + BitXor
     + BitXorAssign
+    + From<usize>
+    + Into<usize>
     + Sized
 {
 }
@@ -77,7 +83,33 @@ impl Div for VirtAddress {
             base: self.base / rhs.base,
         }
     }
+} 
+impl Rem for VirtAddress {
+    type Output = Self;
+    fn rem(self, rhs: Self) -> Self::Output {
+        VirtAddress { base: self.base % rhs.base }
+    }
 }
+
+impl Rem<usize> for VirtAddress {
+   type Output = VirtAddress; 
+   fn rem(self, rhs: usize) -> Self::Output {
+       VirtAddress { base: self.base % rhs }
+   }
+}
+
+impl Into<usize> for VirtAddress {
+    fn into(self) -> usize {
+        self.base  
+    }
+}
+
+impl From<usize> for VirtAddress {
+    fn from(base: usize) -> Self {
+       VirtAddress { base } 
+    }
+}
+
 
 impl MemoryAddress for PhysAddress {}
 
@@ -95,7 +127,30 @@ impl Div for PhysAddress {
     }
 }
 
-fn x(x: VirtAddress) {
-    let y = x * x;
+impl From<usize> for PhysAddress {
+    fn from(base: usize) -> Self {
+        PhysAddress { base } 
+    }
 }
+
+impl Into<usize> for PhysAddress {
+    fn into(self) -> usize {
+        self.base
+    }
+}
+
+impl Rem for PhysAddress {
+    type Output = Self;
+    fn rem(self, rhs: Self) -> Self::Output {
+        PhysAddress { base: self.base % rhs.base }
+    }
+}
+
+impl Rem<usize> for PhysAddress {
+    type Output = Self;
+    fn rem(self, rhs: usize) -> Self::Output {
+        PhysAddress { base: self.base % rhs }
+    }
+}
+
 
